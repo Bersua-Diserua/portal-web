5f9c98e336908918e81745af86a8c41ac6558206
Rizqy Nugroho <rizqy.nugroho@ist.id>
2023-03-26 10:27:37 +0700

    feat: workflow

--- remix.config.js
+++ remix.config.js
@@ -1,9 +1,9 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*"],
   future: {
     unstable_tailwind: true,
   },
-  serverBuildPath: "api/index.js",
   serverDependenciesToBundle: ["axios"],
 };
